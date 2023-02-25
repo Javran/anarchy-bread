8f6568a17f3976cbd2cac05f31832a85fa598d51
Javran Cheng <Javran.C@gmail.com>
2023-02-25 14:55:14 -0800

    factor out model, reduce nesting.

--- src/AnarchyBread/Recipe/Z3.hs
+++ src/AnarchyBread/Recipe/Z3.hs
@@ -102,151 +102,154 @@ maximizeItemLim goal rSet getItem _ =
     . snd
     <$> do
       let logic = Just QF_NIA
-          refs :: [RecipeRef]
-          refs = do
-            (i, inds) <- zip [0 ..] (toList rSet)
-            let item = toEnum @Item i
-            j <- inds
-            pure (item, j)
+          costsAndGains = computeCostGains rSet
 
-      let costsAndGains = computeCostGains rSet
-          itemsInvolved = M.keysSet costsAndGains
+      evalZ3With logic stdOpts $ runModel goal rSet costsAndGains getItem
 
-      evalZ3With logic stdOpts do
-        z <- mkInteger 0
-        -- build up recipe use variables
-        recipeUseVars <-
-          M.fromList <$> forM refs \ref@(item, i) -> do
-            v <- mkFreshIntVar (T.unpack (itemToEmoji item) <> "/" <> show i)
-            assert =<< mkGe v z
-            pure (ref, v)
-        -- item variables
-        itemOutVars <-
-          M.fromList <$> forM (S.toList itemsInvolved) \item -> do
-            v <- mkFreshIntVar (T.unpack (itemToEmoji item) <> "/out")
-            assert =<< mkGe v z
-            pure (item, v)
-        vPenalty <- do
-          v <- mkFreshIntVar "penalty"
-          assert =<< mkGe v z
-          pure v
-        let goalVar = itemOutVars M.! goal
-        forM_ (M.toList costsAndGains) $ \(item, (costs, gains)) -> do
-          let itemIn = fromIntegral (getItem item)
-              outVar = itemOutVars M.! item
-          orig <- mkInteger itemIn
-          totCost <-
-            mkUnaryMinus
-              =<< mkAdd
-              =<< forM costs \(ref, cnt) -> do
+runModel :: Item -> RecipeSet -> M.Map Item CostGain -> (Item -> Int) -> Z3 (Result, Maybe Solution)
+runModel goal rSet costsAndGains getItem = do
+  let refs :: [RecipeRef]
+      refs = do
+        (i, inds) <- zip [0 ..] (toList rSet)
+        let item = toEnum @Item i
+        j <- inds
+        pure (item, j)
+
+      itemsInvolved = M.keysSet costsAndGains
+  z <- mkInteger 0
+  -- build up recipe use variables
+  recipeUseVars <-
+    M.fromList <$> forM refs \ref@(item, i) -> do
+      v <- mkFreshIntVar (T.unpack (itemToEmoji item) <> "/" <> show i)
+      assert =<< mkGe v z
+      pure (ref, v)
+  -- item variables
+  itemOutVars <-
+    M.fromList <$> forM (S.toList itemsInvolved) \item -> do
+      v <- mkFreshIntVar (T.unpack (itemToEmoji item) <> "/out")
+      assert =<< mkGe v z
+      pure (item, v)
+  vPenalty <- do
+    v <- mkFreshIntVar "penalty"
+    assert =<< mkGe v z
+    pure v
+  let goalVar = itemOutVars M.! goal
+  forM_ (M.toList costsAndGains) $ \(item, (costs, gains)) -> do
+    let itemIn = fromIntegral (getItem item)
+        outVar = itemOutVars M.! item
+    orig <- mkInteger itemIn
+    totCost <-
+      mkUnaryMinus
+        =<< mkAdd
+        =<< forM costs \(ref, cnt) -> do
+          let rVar = recipeUseVars M.! ref
+          c <- mkInteger (fromIntegral cnt)
+          mkMul [rVar, c]
+
+    totGain <- do
+      mkAdd =<< forM gains \(ref, cnt) -> do
+        let rVar = recipeUseVars M.! ref
+        c <- mkInteger (fromIntegral cnt)
+        mkMul [rVar, c]
+
+    net <- mkAdd [orig, totCost, totGain]
+    assert =<< mkEq outVar net
+  -- build penalty term.
+  do
+    xs <-
+      mkAdd =<< do
+        forM (M.toList costsAndGains) $ \(item, (costs, _)) -> do
+          ys <- forM costs \(ref, cnt) ->
+            if cnt == 0
+              then pure z
+              else do
+                pFac <- mkInteger (penalty item)
                 let rVar = recipeUseVars M.! ref
-                c <- mkInteger (fromIntegral cnt)
-                mkMul [rVar, c]
+                cnt' <- mkInteger $ fromIntegral cnt
+                mkMul [rVar, pFac, cnt']
+          mkAdd ys
+    assert =<< mkEq vPenalty xs
 
-          totGain <- do
-            mkAdd =<< forM gains \(ref, cnt) -> do
-              let rVar = recipeUseVars M.! ref
-              c <- mkInteger (fromIntegral cnt)
-              mkMul [rVar, c]
+  let initCount = fromIntegral $ getItem goal
+      {-
+        For all current recipes, exactly one item is produced, costing at least one item.
+        therefore it should be a safe assumption that whatever we can produce, it has to be
+        less than this amount.
 
-          net <- mkAdd [orig, totCost, totGain]
-          assert =<< mkEq outVar net
-        -- build penalty term.
-        do
-          xs <-
-            mkAdd =<< do
-              forM (M.toList costsAndGains) $ \(item, (costs, _)) -> do
-                ys <- forM costs \(ref, cnt) ->
-                  if cnt == 0
-                    then pure z
-                    else do
-                      pFac <- mkInteger (penalty item)
-                      let rVar = recipeUseVars M.! ref
-                      cnt' <- mkInteger $ fromIntegral cnt
-                      mkMul [rVar, pFac, cnt']
-                mkAdd ys
-          assert =<< mkEq vPenalty xs
+        Safety of using maximum is based on input assumption: it is only safe
+        when `itemsInvolved` is not empty.
+       -}
+      initHi = 1 + initCount + fromIntegral (maximum (fmap getItem (S.toList itemsInvolved)))
+      initRange :: (Integer, Integer)
+      initRange = (initCount, initHi)
+  {-
+    Z3 maximization is not very reliable for this model,
+    instead we do our own binary search - range = (lo, hi) has the invariant that:
 
-        let initCount = fromIntegral $ getItem goal
-            {-
-              For all current recipes, exactly one item is produced, costing at least one item.
-              therefore it should be a safe assumption that whatever we can produce, it has to be
-              less than this amount.
+    - lo < hi
+    - lo is always satisfiable
+    - hi is never satisfiable
 
-              Safety of using maximum is based on input assumption: it is only safe
-              when `itemsInvolved` is not empty.
-             -}
-            initHi = 1 + initCount + fromIntegral (maximum (fmap getItem (S.toList itemsInvolved)))
-            initRange :: (Integer, Integer)
-            initRange = (initCount, initHi)
-        {-
-          Z3 maximization is not very reliable for this model,
-          instead we do our own binary search - range = (lo, hi) has the invariant that:
+    we can maximize production by testing whether an exact amount is satisfiable.
 
-          - lo < hi
-          - lo is always satisfiable
-          - hi is never satisfiable
+   -}
+  ans <-
+    fix
+      ( \go (lo, hi) -> do
+          let mid = quot (lo + hi) 2
+          if mid <= lo
+            then pure lo
+            else do
+              (_sat, r) <- local do
+                assert =<< mkEq goalVar =<< mkInteger mid
+                withModel \m -> do
+                  Just v <- evalInt m goalVar
+                  pure v
+              case r of
+                Just _ -> go (mid, hi)
+                Nothing -> go (lo, mid)
+      )
+      initRange
+  assert =<< mkEq goalVar =<< mkInteger ans
+  {-
+    optimize recipe use, targeting minimal penalty.
+    note that we no longer have the monotonic property so
+    binary search can't be used, instead we just get current
+    penalty and see if we can force its value down repetitively.
 
-          we can maximize production by testing whether an exact amount is satisfiable.
-
-         -}
-        ans <-
-          fix
-            ( \go (lo, hi) -> do
-                let mid = quot (lo + hi) 2
-                if mid <= lo
-                  then pure lo
-                  else do
-                    (_sat, r) <- local do
-                      assert =<< mkEq goalVar =<< mkInteger mid
-                      withModel \m -> do
-                        Just v <- evalInt m goalVar
-                        pure v
-                    case r of
-                      Just _ -> go (mid, hi)
-                      Nothing -> go (lo, mid)
-            )
-            initRange
-        assert =<< mkEq goalVar =<< mkInteger ans
-        {-
-          optimize recipe use, targeting minimal penalty.
-          note that we no longer have the monotonic property so
-          binary search can't be used, instead we just get current
-          penalty and see if we can force its value down repetitively.
-
-          TODO: evaluate if we need to limit # of iterations before aborting.
-          Z3 seems to be fragile when those repetitive loops are involved.
-         -}
-        ~(Sat, Just initHiP) <- withModel \m -> do
-          ~(Just p) <- evalInt m vPenalty
-          pure p
-        (ansP, iterCount :: Int) <-
-          fix
-            ( \go cur cnt -> do
-                (_sat, r) <- local do
-                  assert =<< mkLt vPenalty =<< mkInteger cur
-                  withModel \m -> do
-                    Just v <- evalInt m vPenalty
-                    pure v
-                case r of
-                  Just v -> go v (cnt + 1)
-                  Nothing -> pure (cur, cnt)
-            )
-            initHiP
-            0
-        liftIO $ do
-          hPutStrLn stderr $ "Optimized after " <> show iterCount <> " iterations."
-          hPutStrLn stderr $ "Penalty improvement (before, after): " <> show (initHiP, ansP)
-        assert =<< mkEq vPenalty =<< mkInteger ansP
-        withModel \m -> do
-          itemChanges :: (M.Map Item (Integer, Integer)) <-
-            M.fromList
-              <$> forM (M.toList itemOutVars) \(item, vOut) -> do
-                ~(Just cntOut) <- evalInt m vOut
-                pure (item, (fromIntegral (getItem item), cntOut))
-          recipeUses :: (M.Map RecipeRef Integer) <-
-            M.fromList
-              <$> forM (M.toList recipeUseVars) \(ref, var) -> do
-                ~(Just v) <- evalInt m var
-                pure (ref, v)
-          pure (itemChanges, recipeUses)
+    TODO: evaluate if we need to limit # of iterations before aborting.
+    Z3 seems to be fragile when those repetitive loops are involved.
+   -}
+  ~(Sat, Just initHiP) <- withModel \m -> do
+    ~(Just p) <- evalInt m vPenalty
+    pure p
+  (ansP, iterCount :: Int) <-
+    fix
+      ( \go cur cnt -> do
+          (_sat, r) <- local do
+            assert =<< mkLt vPenalty =<< mkInteger cur
+            withModel \m -> do
+              Just v <- evalInt m vPenalty
+              pure v
+          case r of
+            Just v -> go v (cnt + 1)
+            Nothing -> pure (cur, cnt)
+      )
+      initHiP
+      0
+  liftIO $ do
+    hPutStrLn stderr $ "Optimized after " <> show iterCount <> " iterations."
+    hPutStrLn stderr $ "Penalty improvement (before, after): " <> show (initHiP, ansP)
+  assert =<< mkEq vPenalty =<< mkInteger ansP
+  withModel \m -> do
+    itemChanges :: (M.Map Item (Integer, Integer)) <-
+      M.fromList
+        <$> forM (M.toList itemOutVars) \(item, vOut) -> do
+          ~(Just cntOut) <- evalInt m vOut
+          pure (item, (fromIntegral (getItem item), cntOut))
+    recipeUses :: (M.Map RecipeRef Integer) <-
+      M.fromList
+        <$> forM (M.toList recipeUseVars) \(ref, var) -> do
+          ~(Just v) <- evalInt m var
+          pure (ref, v)
+    pure (itemChanges, recipeUses)
